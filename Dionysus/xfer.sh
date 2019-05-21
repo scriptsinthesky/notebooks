150fde5de3de23d40da1898915decf59ea49f178
michael.osias <sp4mbucket@gmail.com>
2019-05-21 16:32:00 -0400

    initial commit

---
+++ Dionysus/xfer.sh
@@ -0,0 +1,2 @@
+/usr/local/bin/jupyter-nbconvert --template full --execute --output-dir=/home/pi/course/jupyter/Dionysus /home/pi/course/jupyter/Dionysus/*.ipynb
+/usr/bin/scp *.html quantup1@quantumconfigurations.com:/home1/quantup1/www/dionysus/jupyter
